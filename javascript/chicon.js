0bb792dfb6adbfa0ae7e75644fab4c6e73c8763d
spekulatius <petert@catalyst.net.nz>
2016-02-23 15:15:47 +1300

    Remove trailing spaces in the source code

--- javascript/chicon.js
+++ javascript/chicon.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-	
+
 	jQuery("body").on("click", "#Form_EditForm_action_publish",  function() {
 		var _id = jQuery("#Form_EditForm_ID").val();
 		var _el = jQuery("#record-"+_id);
@@ -29,22 +29,22 @@ jQuery(document).ready(function() {
 	jQuery("body").on("click", "#Form_EditForm_action_unpublish",  function() {
 		var _id = jQuery("#Form_EditForm_ID").val();
 		var _el = jQuery("#record-"+_id);
-		
+
 		_el.removeClass('status-status_published');
 		_el.removeClass("status-status_draft_published");
 		_el.removeClass("status-wfstatus_pub");
 		_el.removeClass("status-wfstatus_att_pub");
 		_el.removeClass("status-wfstatus_att_sup");
 		_el.addClass("status-status_draft");
-		
+
 		var _el = jQuery("#record-"+_id+" li");
 		_el.removeClass('status-status_published');
 		_el.removeClass("status-status-status_draft_published");
 		_el.removeClass("status-wfstatus_pub");
 		_el.removeClass("status-wfstatus_att_pub");
 		_el.removeClass("status-wfstatus_att_sup");
-		_el.addClass("status-status_draft");		
-		
+		_el.addClass("status-status_draft");
+
 		return;
 	})
 
@@ -54,7 +54,7 @@ jQuery(document).ready(function() {
 
 		_el.removeClass("status-status_draft");
 		_el.removeClass("status-status_draft_published");
-		_el.addClass('status-status_published');		
+		_el.addClass('status-status_published');
 	});
 
 });
